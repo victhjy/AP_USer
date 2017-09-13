dd0491ba986bc8ab88dca6eff83b4edf3bba796d
huangjinyang <jinxhuang@huangjinyangdemacbook-pro.local>
2017-09-13 22:57:44 +0800

    other

---
+++ Pods/Target Support Files/BHInfiniteScrollView/BHInfiniteScrollView-umbrella.h
@@ -0,0 +1,20 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "BHInfiniteScrollView.h"
+#import "FXPageControl.h"
+#import "BHInfiniteScrollViewCell.h"
+#import "BHInfiniteScrollViewTitleView.h"
+
+FOUNDATION_EXPORT double BHInfiniteScrollViewVersionNumber;
+FOUNDATION_EXPORT const unsigned char BHInfiniteScrollViewVersionString[];
+
